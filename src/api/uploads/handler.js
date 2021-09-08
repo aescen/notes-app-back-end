66dfbcc2f1878f2d0ccea5aebd0cd4f50213d84c
aescen <ash.putra15@gmail.com>
2021-09-09 00:39:12 +0700

    local storage implemented

---
+++ src/api/uploads/handler.js
@@ -0,0 +1,27 @@
+class UploadsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUploadImagesHandler = this.postUploadImagesHandler.bind(this);
+  }
+
+  async postUploadImagesHandler(request, h) {
+    const { data } = request.payload;
+    this._validator.validateImageHeaders(data.hapi.headers);
+
+    const fileName = await this._service.writeFile(data, data.hapi);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        fileLocation: `http://${process.env.HOST}:${process.env.PORT}/uploads/images/${fileName}`,
+      },
+    });
+    response.code(201);
+
+    return response;
+  }
+}
+
+module.exports = UploadsHandler;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
+const path = require('path');
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
 const Jwt = require('@hapi/jwt');
 const ClientError = require('./exceptions/ClientError');
 
@@ -25,118 +27,137 @@ const AuthenticationsValidator = require('./validator/authentications');
 const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
 
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
-  const usersService = new UsersService();
-  const authenticationsService = new AuthenticationsService();
+  try {
+    const collaborationsService = new CollaborationsService();
+    const notesService = new NotesService(collaborationsService);
+    const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
+    const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/files/images'));
 
-  const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
-    routes: {
-      cors: {
-        origin: ['*'],
+    const server = Hapi.server({
+      port: process.env.PORT,
+      host: process.env.HOST,
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
       },
-    },
-  });
+    });
 
-  // 3rd party plugin
-  await server.register([
-    {
-      plugin: Jwt,
-    },
-  ]);
-
-  // Auth strategies
-  server.auth.strategy('notesapp_jwt', 'jwt', {
-    keys: process.env.ACCESS_TOKEN_KEY,
-    verify: {
-      aud: false,
-      iss: false,
-      sub: false,
-      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
-    },
-    validate: (artifacts) => ({
-      isValid: true,
-      credentials: {
-        id: artifacts.decoded.payload.id,
+    // 3rd party plugin
+    await server.register([
+      {
+        plugin: Jwt,
       },
-    }),
-  });
-
-  // server.auth.default('notesapp_jwt');
-
-  await server.register([
-    {
-      plugin: collaborations,
-      options: {
-        collaborationsService,
-        notesService,
-        validator: CollaborationValidator,
+      {
+        plugin: Inert,
       },
-    },
-    {
-      plugin: notes,
-      options: {
-        service: notesService,
-        validator: NotesValidator,
+    ]);
+
+    // Auth strategies
+    server.auth.strategy('notesapp_jwt', 'jwt', {
+      keys: process.env.ACCESS_TOKEN_KEY,
+      verify: {
+        aud: false,
+        iss: false,
+        sub: false,
+        maxAgeSec: process.env.ACCESS_TOKEN_AGE,
       },
-    },
-    {
-      plugin: users,
-      options: {
-        service: usersService,
-        validator: UsersValidator,
+      validate: (artifacts) => ({
+        isValid: true,
+        credentials: {
+          id: artifacts.decoded.payload.id,
+        },
+      }),
+    });
+
+    // server.auth.default('notesapp_jwt');
+
+    await server.register([
+      {
+        plugin: collaborations,
+        options: {
+          collaborationsService,
+          notesService,
+          validator: CollaborationValidator,
+        },
       },
-    },
-    {
-      plugin: authentications,
-      options: {
-        authenticationsService,
-        usersService,
-        tokenManager: TokenManager,
-        validator: AuthenticationsValidator,
+      {
+        plugin: notes,
+        options: {
+          service: notesService,
+          validator: NotesValidator,
+        },
       },
-    },
-    {
-      plugin: _exports,
-      options: {
-        service: ProducerService,
-        validator: ExportsValidator,
+      {
+        plugin: users,
+        options: {
+          service: usersService,
+          validator: UsersValidator,
+        },
       },
-    },
-  ]);
+      {
+        plugin: authentications,
+        options: {
+          authenticationsService,
+          usersService,
+          tokenManager: TokenManager,
+          validator: AuthenticationsValidator,
+        },
+      },
+      {
+        plugin: _exports,
+        options: {
+          service: ProducerService,
+          validator: ExportsValidator,
+        },
+      },
+      {
+        plugin: uploads,
+        options: {
+          service: storageService,
+          validator: UploadsValidator,
+        },
+      },
+    ]);
 
-  server.ext('onPreResponse', (request, h) => {
-    const { response } = request;
+    server.ext('onPreResponse', (request, h) => {
+      const { response } = request;
 
-    if (response instanceof (ClientError)) {
-      const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
-      });
-      newResponse.code(response.statusCode);
-      return newResponse;
-    }
+      if (response instanceof (ClientError)) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
 
-    // Server Error
-    if (response instanceof (Error) && response.isServer) {
-      console.error(response.stack);
-      const newResponse = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      newResponse.code(500);
-      return newResponse;
-    }
+      // Server Error
+      if (response instanceof (Error) && response.isServer) {
+        console.error(response.stack);
+        const newResponse = h.response({
+          status: 'error',
+          message: 'Maaf, terjadi kegagalan pada server kami.',
+        });
+        newResponse.code(500);
+        return newResponse;
+      }
 
-    return response.continue || response;
-  });
+      return response.continue || response;
+    });
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 init();
